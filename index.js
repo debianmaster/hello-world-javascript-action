1cb73c5408b238919c9bd86a6bf3e55102aaca6f
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-05-24 00:21:51 +0800

    update

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const execa = require('execa');
 try {
   const version = core.getInput('version');
   const kubeconfig_location="/tmp/output/kubeconfig-"+version+".yaml";
-  console.log(`Hello ${version}!`);  
-  execa('docker', ["docker","run","-d","--privileged","--name=k3s-"+version,
+  console.log(`storing kubeconfig here ${kubeconfig_location}!`);  
+  execa('docker', ["run","-d","--privileged","--name=k3s-"+version,
   "-e","K3S_KUBECONFIG_OUTPUT="+kubeconfig_location,
   "-e","K3S_KUBECONFIG_MODE=666",
   "-v","/tmp/output:/tmp/output","-p","6443:6443",
   "rancher/k3s:"+version,"server"]).stdout.pipe(process.stdout)
+  execa('export', ["test=test"]).stdout.pipe(process.stdout)
   core.setOutput("kubeconfig", kubeconfig_location);
 } catch (error) {
   core.setFailed(error.message);
