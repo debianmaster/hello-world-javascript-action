6d3773ecdf346e4ba98a19050df97069875a1b23
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-05-23 23:47:48 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const execa = require('execa');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const version = core.getInput('version');
+  console.log(`Hello ${version}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+  
+  execa('echo', ['unicorns']).stdout.pipe(process.stdout);
+
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
