3e1d2c581cac94fd11ea99fcccbeb0072f6b3f3e
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-05-24 01:23:02 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const execa = require('execa');
+const waitOn = require('wait-on');
 
 try {
   const version = core.getInput('version');
@@ -12,6 +13,20 @@ try {
   "-v","/tmp/output:/tmp/output","-p","6443:6443",
   "rancher/k3s:"+version,"server"]).stdout.pipe(process.stdout)
   core.setOutput("kubeconfig", kubeconfig_location);
+  core.exportVariable('envVar', 'Val');
+  const opts = {
+    resources: [kubeconfig_location],
+    delay: 1000, // initial delay in ms, default 0
+  interval: 100, // poll interval in ms, default 250ms
+  timeout: 30000};
+  waitOn(opts, function (err) {
+    if (err) {
+      console.log("all bad")
+      return;
+    }
+    console.log("all good")
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
