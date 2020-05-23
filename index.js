29bbebcefda0924c3e6608b731c4b158c203ac1c
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-05-24 01:41:08 +0800

    update

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 const fs = require('fs');
 
+async function run() {
+  try {
+    const version = core.getInput('version');
+    const kubeconfig_location="/tmp/output/kubeconfig-"+version+".yaml";
+    console.log(`storing kubeconfig here ${kubeconfig_location}!`); 
+      
+    await exec.exec('docker', ["run","-d","--privileged","--name=k3s-"+version,
+    "-e","K3S_KUBECONFIG_OUTPUT="+kubeconfig_location,
+    "-e","K3S_KUBECONFIG_MODE=666",
+    "-v","/tmp/output:/tmp/output","-p","6443:6443",
+    "rancher/k3s:"+version,"server"]);
 
-try {
-  const version = core.getInput('version');
-  const kubeconfig_location="/tmp/output/kubeconfig-"+version+".yaml";
-  console.log(`storing kubeconfig here ${kubeconfig_location}!`); 
-    
-  await exec.exec('docker', ["run","-d","--privileged","--name=k3s-"+version,
-  "-e","K3S_KUBECONFIG_OUTPUT="+kubeconfig_location,
-  "-e","K3S_KUBECONFIG_MODE=666",
-  "-v","/tmp/output:/tmp/output","-p","6443:6443",
-  "rancher/k3s:"+version,"server"]);
+    await exec.exec('./wait-for-file.sh',kubeconfig_location);
 
-  await exec.exec('./wait-for-file.sh',kubeconfig_location);
+    core.setOutput("kubeconfig", kubeconfig_location);
+    core.exportVariable('envVar', 'Val');
 
-  core.setOutput("kubeconfig", kubeconfig_location);
-  core.exportVariable('envVar', 'Val');
-
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+run();
